bf6d8180ae5e1c9c72e398725d09d57ed6e59fbf
kartik2009 <kartiksingh1998@gmail.com>
2016-11-24 20:51:52 +0530

    Add files via upload

--- js/animations.js
+++ js/animations.js
@@ -8,11 +8,12 @@ var elem31 = document.getElementById('button_container3_1');
 var elem32 = document.getElementById('button_container3_2');
 var elem33 = document.getElementById('button_container3_3');
 var elem4 = document.getElementById('button_container4');
+var elem5 = document.getElementById('button_container5');
 var banner = document.getElementById('flex-banner');
 var dim1= document.getElementById('dim');
 var text= document.getElementById('text-field');
 var hmtxt= document.getElementById('text-home');
-var width=window.innerWidth;
+//var width=window.innerWidth;
 var homelink=document.getElementById('home-link');
 var sci_link=document.getElementById('sci-link');
 var lasya_link=document.getElementById('lasya-link');
@@ -20,6 +21,7 @@ var tech_link=document.getElementById('tech-link');
 var contact_link=document.getElementById('contact-link');
 var bob_link=document.getElementById('bob-link');
 var prosc_link=document.getElementById('prosc-link');
+var reg_link=document.getElementById('reg-link');
 var navinfo=document.getElementById('navinfo');
 elem.onclick = function()
 {
@@ -33,6 +35,7 @@ elem.onclick = function()
 		lasya_link.setAttribute('href', 'lasya.html');
 		bob_link.setAttribute('href', 'bob.html');
 		prosc_link.setAttribute('href', 'proscenium.html');
+		reg_link.setAttribute('href', 'https://www.thecollegefever.com/events/pravega');
 		hmtxt.classList.toggle('final');
 		elem1.classList.toggle('final');
 		elem2.classList.toggle('final');
@@ -42,6 +45,7 @@ elem.onclick = function()
 		elem32.classList.add('final');
 		elem33.classList.add('final');
 		elem4.classList.toggle('final');
+		elem5.classList.toggle('final');
 		banner.classList.toggle('final');
 		dim1.classList.toggle('final');
 		text.classList.toggle('final');
@@ -61,12 +65,14 @@ document.body.onclick = function(event)
 			contact_link.removeAttribute('href');
 			bob_link.removeAttribute('href');
 			prosc_link.removeAttribute('href');
+			reg_link.removeAttribute('href');
 			elem1.classList.toggle('final');
 			elem2.classList.toggle('final');
 			elem3.classList.toggle('final');
 			navinfo.classList.toggle('final');
 			hmtxt.classList.toggle('final');
 			elem4.classList.toggle('final');
+			elem5.classList.toggle('final');
 			banner.classList.toggle('final');
 			dim1.classList.toggle('final');
 			text.classList.toggle('final');
@@ -85,6 +91,7 @@ document.body.onclick = function(event)
 			contact_link.removeAttribute('href');
 			bob_link.removeAttribute('href');
 			prosc_link.removeAttribute('href');
+			reg_link.removeAttribute('href');
 			elem31.classList.toggle('final1');
 			elem32.classList.toggle('final1');
 			elem33.classList.toggle('final1');
@@ -96,6 +103,7 @@ document.body.onclick = function(event)
 			elem2.classList.toggle('final');
 			elem3.classList.toggle('final');
 			elem4.classList.toggle('final');
+			elem5.classList.toggle('final');
 			banner.classList.toggle('final');
 			dim1.classList.toggle('final');
 			text.classList.toggle('final');
